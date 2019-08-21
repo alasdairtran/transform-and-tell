3cd0ae7c972dd639207573a947ff2a01e4c84976
Alasdair Tran <alasdair.tran@gmail.com>
2019-08-21 14:53:08 +1000

    Add GoodNewsReader

--- newser/data/dataset_readers/__init__.py
+++ newser/data/dataset_readers/__init__.py
@@ -1,1 +1,2 @@
+from .goodnews import GoodNewsReader
 from .nytimes import NYTimesReader
--- newser/data/dataset_readers/goodnews.py
+++ newser/data/dataset_readers/goodnews.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import random
-from datetime import datetime
 from typing import Dict
 
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
@@ -20,9 +19,9 @@ from newser.data.fields import ImageField
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-@DatasetReader.register('nytimes')
-class NYTimesReader(DatasetReader):
-    """Read from the New York Times dataset.
+@DatasetReader.register('goodnews')
+class GoodNewsReader(DatasetReader):
+    """Read from the Good News dataset.
 
     See the repo README for more instruction on how to download the dataset.
 
@@ -47,7 +46,7 @@ class NYTimesReader(DatasetReader):
         self._tokenizer = tokenizer
         self._token_indexers = token_indexers
         self.client = MongoClient(host=mongo_host, port=mongo_port)
-        self.db = self.client.nytimes
+        self.db = self.client.goodnews
         self.image_dir = image_dir
         self.preprocess = Compose([
             Resize(256), CenterCrop(224), ToTensor(),
@@ -57,49 +56,36 @@ class NYTimesReader(DatasetReader):
     @overrides
     def _read(self, split: str):
         # split can be either train, valid, or test
-        # validation and test sets contain 10K examples each
-        if split == 'train':
-            start = datetime(1980, 1, 1)
-            end = datetime(2018, 7, 1)
-        elif split == 'valid':
-            start = datetime(2018, 7, 1)
-            end = datetime(2018, 10, 1)
-        elif split == 'test':
-            start = datetime(2018, 10, 1)
-            end = datetime(2019, 1, 1)
-        else:
+        if split not in ['train', 'val', 'test']:
             raise ValueError(f'Unknown split: {split}')
 
         # Setting the batch size is needed to avoid cursor timing out
-        article_cursor = self.db.articles.find({
-            'article': {'$exists': True, '$ne': ''},  # non-empty article body
-            'images': {'$exists': True, '$ne': {}},  # at least one image
-            'pub_date': {'$gte': start, '$lt': end},
+        sample_cursor = self.db.splits.find({
+            'split': {'$eq': split},
         }, no_cursor_timeout=True).batch_size(128)
 
-        for article in article_cursor:
-            # Ensure caption is non-empty
-            if '0' not in article['images'] or not article['images']['0'].strip():
-                continue
+        for sample in sample_cursor:
+            # Find the corresponding article
+            article = self.db.articles.find_one({
+                '_id': {'$eq': sample['article_id']},
+            })
 
-            # Get the top image of the article
-            image_name = f"{article['_id']}_0.jpg"
-            image_path = os.path.join(self.image_dir, image_name)
+            # Load the image
+            image_path = os.path.join(self.image_dir, f"{sample['_id']}.jpg")
             try:
                 with Image.open(image_path) as image:
-                    if image.size[0] < 256 or image.size[1] < 256:
-                        continue
                     image = image.convert('RGB')
-            except (FileNotFoundError, OSError):
+            except FileNotFoundError:
                 continue
 
-            caption = article['images']['0']
+            caption = article['images'][sample['image_index']]
             yield self.article_to_instance(article, image, caption)
 
-        article_cursor.close()
+        sample_cursor.close()
 
     def article_to_instance(self, article, image, caption) -> Instance:
-        context = article['article']
+        context = article['article'].strip()
+        caption = caption.strip()
 
         context_tokens = self._tokenizer.tokenize(context)
         caption_tokens = self._tokenizer.tokenize(caption)
