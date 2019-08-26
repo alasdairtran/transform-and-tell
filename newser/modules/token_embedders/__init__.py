665db33c8eb7b4759d376e3ea68e4db3b66e86b2
Alasdair Tran <alasdair.tran@gmail.com>
2019-08-26 15:04:09 +1000

    Add SumTextFieldEmbedder

--- newser/modules/token_embedders/__init__.py
+++ newser/modules/token_embedders/__init__.py
@@ -1,3 +1,4 @@
 from .adaptive import AdaptiveEmbedding
 from .positional import (LearnedPositionalEmbedding,
                          SinusoidalPositionalEmbedding)
+from .sum_text_field_embedder import SumTextFieldEmbedder
