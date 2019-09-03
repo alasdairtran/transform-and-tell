7169e216ed2415916b173161d5b42f9c7cf0fa15
Alasdair Tran <alasdair.tran@gmail.com>
2019-09-03 13:47:24 +1000

    Add TransformerPointerModel

--- newser/models/__init__.py
+++ newser/models/__init__.py
@@ -2,3 +2,4 @@ from .baseline import BaselineModel
 from .pointer import PointerModel
 from .transformer import TransformerModel
 from .transformer_flattened import TransformerFlattenedModel
+from .transformer_pointer import TransformerPointerModel
