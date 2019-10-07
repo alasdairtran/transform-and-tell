63d3654ff3a8fd5360390607ddf65c67b0af27ea
Alasdair Tran <alasdair.tran@gmail.com>
2019-10-07 22:07:08 +1100

    Update stats computation

--- scripts/compute_data_statistics.py
+++ scripts/compute_data_statistics.py
@@ -8,24 +8,27 @@ Options:
 
 """
 import os
+from datetime import datetime
 from glob import glob
 
 import ptvsd
 import torchvision.transforms.functional as F
 from docopt import docopt
 from PIL import Image
+from pymongo import MongoClient
 from schema import And, Or, Schema, Use
 from tqdm import tqdm
-from pymongo import MongoClient
+
 from newser.utils import setup_logger
-from datetime import datetime
+
 logger = setup_logger()
 
 
 def compute_nytimes_stats(nytimes):
     cursor = nytimes.articles.find({
-        'parsed': True, # article body is parsed into paragraphs
-        'n_images': {'$gt': 0}, # at least one image is present
+        'parsed': True,  # article body is parsed into paragraphs
+        'n_images': {'$gt': 0},  # at least one image is present
+        'language': 'en',
     })
 
     n_article_words = 0
@@ -41,11 +44,11 @@ def compute_nytimes_stats(nytimes):
         sections = article['parsed_section']
         pars = [s['text'] for s in sections if s['type'] == 'paragraph']
         captions = []
-        
+
         for s in sections:
             if s['type'] == 'caption':
                 image_path = os.path.join('data/nytimes/images_processed',
-                                            f"{s['hash']}.jpg")
+                                          f"{s['hash']}.jpg")
                 if os.path.exists(image_path):
                     captions.append(s['text'])
         if not captions:
@@ -55,7 +58,7 @@ def compute_nytimes_stats(nytimes):
         n_articles += 1
         n_caption_words += len(' '.join(captions).split())
         n_captions += len(captions)
-        
+
         if article['pub_date'] < min_date:
             min_date = article['pub_date']
         if article['pub_date'] > max_date:
@@ -70,10 +73,11 @@ def compute_nytimes_stats(nytimes):
     print('Max date:', max_date)
     print()
 
+
 def compute_nytimes_subset_statistics(nytimes):
     cursor = nytimes.articles.find({
-        'parsed': True, # article body is parsed into paragraphs
-        'n_images': {'$gt': 0}, # at least one image is present
+        'parsed': True,  # article body is parsed into paragraphs
+        'n_images': {'$gt': 0},  # at least one image is present
         'pub_date': {'$gte': datetime(2010, 1, 1), '$lt': datetime(2018, 7, 1)}
     })
 
@@ -90,11 +94,11 @@ def compute_nytimes_subset_statistics(nytimes):
         sections = article['parsed_section']
         pars = [s['text'] for s in sections if s['type'] == 'paragraph']
         captions = []
-        
+
         for s in sections:
             if s['type'] == 'caption':
                 image_path = os.path.join('data/nytimes/images_processed',
-                                            f"{s['hash']}.jpg")
+                                          f"{s['hash']}.jpg")
                 if os.path.exists(image_path):
                     captions.append(s['text'])
         if not captions:
@@ -104,7 +108,7 @@ def compute_nytimes_subset_statistics(nytimes):
         n_articles += 1
         n_caption_words += len(' '.join(captions).split())
         n_captions += len(captions)
-        
+
         if article['pub_date'] < min_date:
             min_date = article['pub_date']
         if article['pub_date'] > max_date:
@@ -118,6 +122,7 @@ def compute_nytimes_subset_statistics(nytimes):
     print('Min date:', min_date)
     print('Max date:', max_date)
     print()
+
 
 def compute_nytimes_exact_subset_statistics(nytimes, goodnews):
     cursor = goodnews.splits.find({}, no_cursor_timeout=True)
@@ -136,14 +141,14 @@ def compute_nytimes_exact_subset_statistics(nytimes, goodnews):
         article = goodnews.articles.find_one({
             '_id': {'$eq': sample['article_id']},
         })
-        
+
         if sample['article_id'] not in article_ids:
             article_ids.add(sample['article_id'])
             article = nytimes.articles.find_one({'_id': sample['article_id']})
 
             if article is None or 'parsed_section' not in article:
                 continue
-    
+
             sections = article['parsed_section']
             pars = [s['text'] for s in sections if s['type'] == 'paragraph']
             captions = []
@@ -161,7 +166,7 @@ def compute_nytimes_exact_subset_statistics(nytimes, goodnews):
             n_articles += 1
             n_caption_words += len(' '.join(captions).split())
             n_captions += len(captions)
-            
+
             if article['pub_date'] < min_date:
                 min_date = article['pub_date']
             if article['pub_date'] > max_date:
@@ -176,6 +181,7 @@ def compute_nytimes_exact_subset_statistics(nytimes, goodnews):
     print('Max date:', max_date)
     print()
 
+
 def compute_goodnews_stats(goodnews):
     cursor = goodnews.splits.find({})
     article_ids = set({})
@@ -189,14 +195,15 @@ def compute_goodnews_stats(goodnews):
             '_id': {'$eq': sample['article_id']},
         })
 
-        image_path = os.path.join('data/goodnews/images_processed', f"{sample['_id']}.jpg")
+        image_path = os.path.join(
+            'data/goodnews/images_processed', f"{sample['_id']}.jpg")
         if not os.path.exists(image_path):
             continue
 
         if sample['article_id'] not in article_ids:
             article_ids.add(sample['article_id'])
             n_article_words += len(article['article'].strip().split())
-            
+
         caption = article['images'][sample['image_index']].strip()
         n_caption_words += len(caption.split())
         n_captions += 1
@@ -234,7 +241,7 @@ def main():
     goodnews = client.goodnews
 
     # compute_goodnews_stats(goodnews)
-    # compute_nytimes_stats(nytimes)
+    compute_nytimes_stats(nytimes)
     # compute_nytimes_subset_statistics(nytimes)
     compute_nytimes_exact_subset_statistics(nytimes, goodnews)
 
