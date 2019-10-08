ca2eceda1c26c13b4ef30ddfab1b71fa639517e4
Alasdair Tran <alasdair.tran@gmail.com>
2019-10-08 15:23:37 +1100

    Annotate parts of speech

--- scripts/annotate_nytimes_corefs.py
+++ scripts/annotate_nytimes_corefs.py
@@ -11,6 +11,7 @@ Options:
 import ptvsd
 import spacy
 from docopt import docopt
+from joblib import Parallel, delayed
 from pymongo import MongoClient
 from schema import And, Or, Schema, Use
 from tqdm import tqdm
@@ -45,12 +46,20 @@ def get_clusters(doc, article):
         start = main_span.start_char
         end = main_span.end_char
 
+        pos = []
+        for tok in main_span:
+            pos.append({
+                'start': tok.idx,
+                'end': tok.idx + len(tok.text),
+                'text': tok.text,
+                'pos': tok.pos_,
+            })
         kept_cluster = {
             'main': {
-                'text': main_span.text,
                 'start': start,
                 'end': end,
-                'pos': [span.pos_ for span in main_span],
+                'text': main_span.text,
+                'pos': pos,
             },
             'mentions': [],
         }
@@ -63,11 +72,19 @@ def get_clusters(doc, article):
             if s == start and e == end:
                 continue
 
+            pos = []
+            for tok in main_span:
+                pos.append({
+                    'start': tok.idx,
+                    'end': tok.idx + len(tok.text),
+                    'text': tok.text,
+                    'pos': tok.pos_,
+                })
             mention = {
-                'text': coref.text,
                 'start': s,
                 'end': e,
-                'pos': [span.pos_ for span in coref],
+                'text': coref.text,
+                'pos': pos,
             }
 
             kept_cluster['mentions'].append(mention)
@@ -92,14 +109,56 @@ def assign_coref_to_section(section, coref, i, kind, mention_idx):
     s = section['spacy_start']
     e = section['spacy_end']
     if coref['start'] >= s and coref['end'] <= e:
+        pos = []
+        for o_pos in coref['pos']:
+            pos.append({
+                'start': o_pos['start'] - s,
+                'end': o_pos['end'] - s,
+                'text': o_pos['text'],
+                'pos': o_pos['pos'],
+            })
+
         section['corefs'].append({
             'cluster_idx': i,
             'kind': kind,
             'mention_index': mention_idx,
+            'start': coref['start'] - s,
+            'end': coref['end'] - s,
             'text': coref['text'],
-            'start': coref['start'] - section['spacy_start'],
-            'end': coref['end'] - section['spacy_start'],
-            'pos': coref['pos']
+            'pos':  pos,
+        })
+
+
+def get_parts_of_speech(doc, article):
+    parts_of_speech = []
+    for tok in doc:
+        pos = {
+            'start': tok.idx,
+            'end': tok.idx + len(tok.text),  # exclude right endpoint
+            'text': tok.text,
+            'pos': tok.pos_,
+        }
+        parts_of_speech.append(pos)
+
+        if 'main' in article['headline']:
+            section = article['headline']
+            assign_pos_to_section(section, pos)
+
+        for section in article['parsed_section']:
+            assign_pos_to_section(section, pos)
+
+    article['parts_of_speech'] = parts_of_speech
+
+
+def assign_pos_to_section(section, pos):
+    s = section['spacy_start']
+    e = section['spacy_end']
+    if pos['start'] >= s and pos['end'] <= e:
+        section['parts_of_speech'].append({
+            'start': pos['start'] - s,
+            'end': pos['end'] - s,
+            'text': pos['text'],
+            'pos':  pos['pos'],
         })
 
 
@@ -112,6 +171,7 @@ def calculate_spacy_positions(article):
         cursor += len(title) + 1  # newline
         article['headline']['spacy_end'] = cursor
         article['headline']['corefs'] = []
+        article['headline']['parts_of_speech'] = []
 
     for section in article['parsed_section']:
         text = section['text'].strip()
@@ -119,6 +179,32 @@ def calculate_spacy_positions(article):
         cursor += len(text) + 1  # newline
         section['spacy_end'] = cursor
         section['corefs'] = []
+        section['parts_of_speech'] = []
+
+
+def parse_article(article, nlp, db):
+    if 'parts_of_speech' in article['parsed_section'][0]:
+        return
+
+    calculate_spacy_positions(article)
+
+    title = ''
+    if 'main' in article['headline']:
+        title = article['headline']['main'].strip()
+
+    sections = article['parsed_section']
+
+    paragraphs = [s['text'].strip() for s in sections]
+    paragraphs = [title] + paragraphs
+
+    combined = '\n'.join(paragraphs)
+
+    doc = nlp(combined)
+    get_clusters(doc, article)
+    get_parts_of_speech(doc, article)
+
+    db.articles.find_one_and_update(
+        {'_id': article['_id']}, {'$set': article})
 
 
 def main():
@@ -143,28 +229,9 @@ def main():
     }, no_cursor_timeout=True).batch_size(128)
 
     logger.info('Annotating articles.')
-    for article in tqdm(article_cursor):
-        # if 'coref_clusters' in article:
-        #     continue
-
-        calculate_spacy_positions(article)
-
-        title = ''
-        if 'main' in article['headline']:
-            title = article['headline']['main'].strip()
-
-        sections = article['parsed_section']
-
-        paragraphs = [s['text'].strip() for s in sections]
-        paragraphs = [title] + paragraphs
-
-        combined = '\n'.join(paragraphs)
-
-        doc = nlp(combined)
-        get_clusters(doc, article)
-
-        db.articles.find_one_and_update(
-            {'_id': article['_id']}, {'$set': article})
+    with Parallel(n_jobs=4, backend='threading') as parallel:
+        parallel(delayed(parse_article)(article, nlp, db)
+                 for article in tqdm(article_cursor))
 
 
 if __name__ == '__main__':
