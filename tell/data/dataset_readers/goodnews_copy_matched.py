Fix caption name leakage during generation

tell/data/dataset_readers/goodnews_copy_matched.py
@@ -124,10 +124,11 @@ class GoodNewsCopyMatchedReader(DatasetReader):
                 article, sample['image_index'])
 
             self._process_copy_tokens(copy_infos, article)
+            proper_infos = self._get_context_names(article)
 
-            yield self.article_to_instance(article, named_entities, face_embeds, image, sample['image_index'], image_path, copy_infos)
+            yield self.article_to_instance(article, named_entities, face_embeds, image, sample['image_index'], image_path, copy_infos, proper_infos)
 
-    def article_to_instance(self, article, named_entities, face_embeds, image, image_index, image_path, copy_infos) -> Instance:
+    def article_to_instance(self, article, named_entities, face_embeds, image, image_index, image_path, copy_infos, proper_infos) -> Instance:
         context = article['context'].strip()
 
         caption = article['images'][image_index]
@@ -147,10 +148,10 @@ class GoodNewsCopyMatchedReader(DatasetReader):
             name_field = stub_field.empty_field()
 
         fields = {
-            'context': CopyTextField(context_tokens, self._token_indexers, copy_infos, 'context'),
+            'context': CopyTextField(context_tokens, self._token_indexers, copy_infos, proper_infos, 'context'),
             'names': ListTextField(name_field),
             'image': ImageField(image, self.preprocess),
-            'caption': CopyTextField(caption_tokens, self._token_indexers, copy_infos, 'caption'),
+            'caption': CopyTextField(caption_tokens, self._token_indexers, copy_infos, None, 'caption'),
             'face_embeds': ArrayField(face_embeds, padding_value=np.nan),
         }
 
@@ -201,6 +202,22 @@ class GoodNewsCopyMatchedReader(DatasetReader):
                     })
                 else:
                     copy_infos[pos['text']]['caption'].append(
+                        (pos['start'], pos['end']))
+
+        return copy_infos
+
+    def _get_context_names(self, article):
+        copy_infos = {}
+
+        context_pos = article['context_parts_of_speech']
+        for pos in context_pos:
+            if pos['pos'] == 'PROPN':
+                if pos['text'] not in copy_infos:
+                    copy_infos[pos['text']] = OrderedDict({
+                        'context': [(pos['start'], pos['end'])]
+                    })
+                else:
+                    copy_infos[pos['text']]['context'].append(
                         (pos['start'], pos['end']))
 
         return copy_infos

tell/data/dataset_readers/nytimes_copy_matched.py
@@ -181,11 +181,13 @@ class NYTimesCopyMatchedReader(DatasetReader):
                 paragraphs = paragraphs + before + after
                 pos_pars = pos_pars + before_pos + after_pos
                 self._process_copy_tokens(copy_infos, paragraphs, pos_pars)
+                proper_infos = self._get_context_names(
+                    paragraphs, pos_pars)
                 named_entities = sorted(named_entities)
 
-                yield self.article_to_instance(copy_infos, paragraphs, named_entities, image, caption, image_path, article['web_url'], pos, face_embeds)
+                yield self.article_to_instance(copy_infos, proper_infos, paragraphs, named_entities, image, caption, image_path, article['web_url'], pos, face_embeds)
 
-    def article_to_instance(self, copy_infos, paragraphs, named_entities, image, caption, image_path, web_url, pos, face_embeds) -> Instance:
+    def article_to_instance(self, copy_infos, proper_infos, paragraphs, named_entities, image, caption, image_path, web_url, pos, face_embeds) -> Instance:
         context = '\n'.join(paragraphs).strip()
 
         context_tokens = self._tokenizer.tokenize(context)
@@ -201,10 +203,10 @@ class NYTimesCopyMatchedReader(DatasetReader):
             name_field = stub_field.empty_field()
 
         fields = {
-            'context': CopyTextField(context_tokens, self._token_indexers, copy_infos, 'context'),
+            'context': CopyTextField(context_tokens, self._token_indexers, copy_infos, proper_infos, 'context'),
             'names': ListTextField(name_field),
             'image': ImageField(image, self.preprocess),
-            'caption': CopyTextField(caption_tokens, self._token_indexers, copy_infos, 'caption'),
+            'caption': CopyTextField(caption_tokens, self._token_indexers, copy_infos, None, 'caption'),
             'face_embeds': ArrayField(face_embeds, padding_value=np.nan),
         }
 
@@ -266,6 +268,23 @@ class NYTimesCopyMatchedReader(DatasetReader):
                 else:
                     copy_infos[pos['text']]['caption'].append(
                         (pos['start'], pos['end']))
+
+        return copy_infos
+
+    def _get_context_names(self, paragraphs, pos_pars):
+        offset = 0
+        copy_infos = {}
+        for par, pos_par in zip(paragraphs, pos_pars):
+            for pos in pos_par:
+                if pos['pos'] == 'PROPN':
+                    if pos['text'] not in copy_infos:
+                        copy_infos[pos['text']] = OrderedDict({
+                            'context': [(pos['start'] + offset, pos['end'] + offset)],
+                        })
+                    else:
+                        copy_infos[pos['text']]['context'].append(
+                            (pos['start'] + offset, pos['end'] + offset))
+            offset += len(par) + 1
 
         return copy_infos
 