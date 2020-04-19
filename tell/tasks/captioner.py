32f9ef1f44bc835406afb65caab1efc2a6fcf4df
Alasdair Tran <alasdair.tran@gmail.com>
2020-04-20 00:25:33 +1000

    Support object embeddings in demo

--- tell/tasks/captioner.py
+++ tell/tasks/captioner.py
@@ -1,9 +1,12 @@
 import base64
 import io
 import logging
+import os
+import random
 import re
 from collections import OrderedDict
 
+import cv2
 import numpy as np
 import spacy
 import torch
@@ -22,8 +25,13 @@ from torchvision.transforms import (CenterCrop, Compose, Normalize, Resize,
                                     ToTensor)
 
 from tell.commands.train import yaml_to_params
-from tell.data.fields import CopyTextField, ImageField
+from tell.data.fields import ImageField
 from tell.facenet import MTCNN, InceptionResnetV1
+from tell.models.resnet import resnet152
+from tell.yolov3.models import Darknet, attempt_download
+from tell.yolov3.utils.datasets import letterbox
+from tell.yolov3.utils.utils import (load_classes, non_max_suppression,
+                                     plot_one_box, scale_coords)
 
 from .base import Worker
 
@@ -49,7 +57,11 @@ class CaptioningWorker(Worker):
         self.tokenizer = None
         self.token_indexers = None
         self.mtcnn = None
+        self.inception = None
         self.resnet = None
+        self.darknet = None
+        self.names = None
+        self.colors = None
         self.nlp = None
         if torch.cuda.is_available():
             self.device = torch.device('cuda:0')
@@ -59,7 +71,7 @@ class CaptioningWorker(Worker):
     def initialize(self):
         # We need to initialize the model inside self.run and not self.__init__
         # to ensure that the model loads in the correct thread.
-        config_path = 'expt/nytimes/8_transformer_faces/config.yaml'
+        config_path = 'expt/nytimes/9_transformer_objects/config.yaml'
         logger.info(f'Loading config from {config_path}')
         config = yaml_to_params(config_path, overrides='')
         prepare_environment(config)
@@ -67,7 +79,7 @@ class CaptioningWorker(Worker):
         model = Model.from_params(vocab=vocab, params=config.pop('model'))
         model = model.eval().to(self.device)
 
-        model_path = 'expt/nytimes/8_transformer_faces/serialization/best.th'
+        model_path = 'expt/nytimes/9_transformer_objects/serialization/best.th'
         logger.info(f'Loading best model from {model_path}')
         best_model_state = torch.load(model_path, map_location=self.device)
         model.load_state_dict(best_model_state)
@@ -81,7 +93,24 @@ class CaptioningWorker(Worker):
 
         logger.info('Loading face detection model.')
         self.mtcnn = MTCNN(keep_all=True, device=self.device)
-        self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
+        self.inception = InceptionResnetV1(pretrained='vggface2').eval()
+
+        self.resnet = resnet152()
+        self.resnet = self.resnet.to(self.device).eval()
+
+        cfg = 'tell/yolov3/cfg/yolov3-spp.cfg'
+        weight_path = 'data/yolov3-spp-ultralytics.pt'
+        self.darknet = Darknet(cfg, img_size=416)
+        attempt_download(weight_path)
+        self.darknet.load_state_dict(torch.load(
+            weight_path, map_location=self.device)['model'])
+        self.darknet.to(self.device).eval()
+
+        # Get names and colors
+        self.names = load_classes('tell/yolov3/data/coco.names')
+        random.seed(123)
+        self.colors = [[random.randint(0, 255) for _ in range(3)]
+                       for _ in range(len(self.names))]
 
         self.preprocess = Compose([
             Resize(256),
@@ -112,8 +141,7 @@ class CaptioningWorker(Worker):
         for batch in iterator:
             if self.device.type == 'cuda':
                 batch = move_to_device(batch, self.device.index)
-            with torch.no_grad():
-                output_dict = self.model.generate(**batch)
+            output_dict = self.model.generate(**batch)
             generated_captions += output_dict['generations']
 
         output = []
@@ -142,6 +170,7 @@ class CaptioningWorker(Worker):
             'context': TextField(context_tokens, self.token_indexers),
             'image': ImageField(sample['image'], self.preprocess),
             'face_embeds': ArrayField(sample['face_embeds'], padding_value=np.nan),
+            'obj_embeds': ArrayField(sample['obj_embeds'], padding_value=np.nan),
         }
 
         metadata = {
@@ -224,6 +253,7 @@ class CaptioningWorker(Worker):
         image = Image.open(io.BytesIO(image_data))
         image = image.convert('RGB')
         face_embeds = self.get_faces(image)
+        obj_embeds = self.get_objects(image)
 
         output = {
             'paragraphs': paragraphs + before + after,
@@ -233,6 +263,7 @@ class CaptioningWorker(Worker):
             'after': after,
             'image': image,
             'face_embeds': face_embeds,
+            'obj_embeds': obj_embeds,
         }
 
         return output
@@ -248,8 +279,67 @@ class CaptioningWorker(Worker):
             if faces is None:
                 return np.array([[]])
 
-            embeddings, _ = self.resnet(faces)
+            embeddings, _ = self.inception(faces)
             return embeddings.cpu().numpy()[:4]
+
+    def get_objects(self, image):
+        im0 = np.array(image)
+        img = letterbox(im0, new_shape=416)[0]
+        img = img.transpose(2, 0, 1)  # to 3x416x416
+        img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img).to(self.device)
+        img = img.float()
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        pred = self.darknet(img)[0]
+
+        # Apply NMS
+        # We ignore the person class (class 0)
+        pred = non_max_suppression(pred, 0.3, 0.6,
+                                   classes=None, agnostic=False)
+
+        # Process detections
+        assert len(pred) == 1, f'Length of pred is {len(pred)}'
+        det = pred[0]
+
+        im0 = im0[:, :, ::-1]  # to BGR
+        im0 = np.ascontiguousarray(im0)
+
+        obj_feats = []
+        confidences = []
+        classes = []
+        if det is not None and len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(
+                img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+
+            # Write results
+            for j, (*xyxy, conf, class_) in enumerate(det):
+                if j >= 64:
+                    break
+
+                obj_feat = get_obj_embeddings(
+                    xyxy, image, None, self.resnet)
+                obj_feats.append(obj_feat)
+                confidences.append(conf.item())
+                classes.append(int(class_))
+
+                label = '%s %.2f' % (self.names[int(class_)], conf)
+                plot_one_box(xyxy, im0, label=label,
+                             color=self.colors[int(class_)])
+
+        # Save results (image with detections)
+        # cv2.imwrite(save_path, im0)
+
+        if not obj_feats:
+            return np.array([[]])
+        return np.array(obj_feats)
 
     def to_token_ids(self, sentence):
         bpe_tokens = self.bpe.encode(sentence)
@@ -264,9 +354,71 @@ class CaptioningWorker(Worker):
     @overrides
     def _process(self, job):
         articles = job['message']
-        output = self.generate_captions(articles)
+        with torch.no_grad():
+            output = self.generate_captions(articles)
 
         return {
             'client_id': job['client_id'],
             'output': output,
         }
+
+
+def get_obj_embeddings(xyxy, pil_image, obj_path, resnet):
+    pil_image = pil_image.convert('RGB')
+    obj_image = extract_object(pil_image, xyxy, save_path=obj_path)
+
+    preprocess = Compose([
+        ToTensor(),
+        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    obj_image = preprocess(obj_image)
+    # obj_image.shape == [n_channels, height, width]
+
+    # Add a batch dimension
+    obj_image = obj_image.unsqueeze(0).to(next(resnet.parameters()).device)
+    # obj_image.shape == [1, n_channels, height, width]
+
+    X_image = resnet(obj_image, pool=True)
+    # X_image.shape == [1, 2048]
+
+    X_image = X_image.squeeze(0).cpu().numpy().tolist()
+    # X_image.shape == [2048]
+
+    return X_image
+
+
+def extract_object(img, box, image_size=224, margin=0, save_path=None):
+    """Extract object + margin from PIL Image given bounding box.
+
+    Arguments:
+        img {PIL.Image} -- A PIL Image.
+        box {numpy.ndarray} -- Four-element bounding box.
+        image_size {int} -- Output image size in pixels. The image will be square.
+        margin {int} -- Margin to add to bounding box, in terms of pixels in the final image.
+            Note that the application of the margin differs slightly from the davidsandberg/facenet
+            repo, which applies the margin to the original image before resizing, making the margin
+            dependent on the original image size.
+        save_path {str} -- Save path for extracted object image. (default: {None})
+
+    Returns:
+        torch.tensor -- tensor representing the extracted object.
+    """
+    margin = [
+        margin * (box[2] - box[0]) / (image_size - margin),
+        margin * (box[3] - box[1]) / (image_size - margin)
+    ]
+    box = [
+        int(max(box[0] - margin[0]/2, 0)),
+        int(max(box[1] - margin[1]/2, 0)),
+        int(min(box[2] + margin[0]/2, img.size[0])),
+        int(min(box[3] + margin[1]/2, img.size[1]))
+    ]
+
+    obj = img.crop(box).resize((image_size, image_size), 2)
+
+    if save_path is not None:
+        os.makedirs(os.path.dirname(save_path)+'/', exist_ok=True)
+        save_args = {'compress_level': 0} if '.png' in save_path else {}
+        obj.save(save_path, **save_args)
+
+    return obj
