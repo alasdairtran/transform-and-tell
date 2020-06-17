0e57c72a07f1e9ee26f66709f5a4778ca5bd77da
Alasdair Tran <alasdair.tran@gmail.com>
2020-06-17 15:42:45 +1000

    Set ALLOWED_HOSTS

--- demo/backend/backend/settings.py
+++ demo/backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.transform-and-tell.ml']
 
 
 # Application definition
